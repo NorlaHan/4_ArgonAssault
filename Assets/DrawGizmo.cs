3265076226642b42a406d39010af654ef426993c
PhhP <ph_430@hotmail.com>
2018-07-18 09:55:03 +0800

    Rough terrain,skybox, player with control , rail animtion, weapon effect, explosion effect.

---
+++ Assets/DrawGizmo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmo : MonoBehaviour {
+
+    [SerializeField] Color color = Color.green;
+    [SerializeField] Vector3 size =new Vector3(2,2,2);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere (transform.position, size.x);
+    }
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] int startCount = 5;
+    public GameObject musicPlayerPrefab;
+
+    MusicPlayer musicPlayer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // If MusicPlayer doesn't exist in any cases. Instantiate from the prefab.
+        if (!GameObject.FindObjectOfType<MusicPlayer>())
+        {
+            musicPlayer = Instantiate(musicPlayerPrefab).GetComponent<MusicPlayer>();
+        }
+        else {
+            musicPlayer = GameObject.FindObjectOfType<MusicPlayer>();
+        }
+
+        // Only works on splash screen
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Invoke("OnLoadLevel01", startCount);
+            InvokeRepeating("CountSeconds", 1, 1);
+        }        
+    }
+
+    private void OnLoadLevel01()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void CountSeconds() {
+        startCount--;
+        if (startCount == 0)
+        {
+            print("Game START!");
+        }
+        else {
+            print(startCount);
+        }        
+    }
+}
