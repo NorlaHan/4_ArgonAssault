58b4f7deeb6a5968a80fb2ea52ea4f53e44b3ba4
PhhP <ph_430@hotmail.com>
2018-07-30 17:45:02 +0800

    Add enemies assets. player collide with other objects

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // Ok as ling as this is the only script that loads scene
+
+public class CollisionHandler : MonoBehaviour {
+
+    [Tooltip("In seconds")][SerializeField] float levelDelay = 1f;
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathFX;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemies")
+        {
+            print("Player Hit enemies");
+        }
+        else
+        {
+            print("Player Hit " + other.name);
+        }
+        StartDeathSequence();
+        
+    }
+
+    private void StartDeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("OnReloadScene",levelDelay);
+    }
+
+    private void OnReloadScene()    // String reference
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(name + ", hit " + collision.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+// TODO Player collide with environment and enemies.
+// TODO Player shoots bullet.
+// TODO Fix the bullet slower than player.
+// TODO enemies action.
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("General")]    
+    [SerializeField] [Tooltip("In ms^-1")] float xControlSpeed = 8f;
+    [SerializeField][Tooltip("In ms^-1")] float yControlSpeed = 8f;
+    [SerializeField][Tooltip("In m")] float xRange = 5f;
+    [SerializeField] [Tooltip("In m")] float yRange = 3f;
+
+    [Header("Screen-position Based")]
+    [SerializeField] float posPitchFactor = 2f;
+    [SerializeField] float posYawFactor = -2f;
+
+    [Header("Control-throw Based")]
+    [SerializeField] float ctrlPitchFactor = -20f;
+    [SerializeField] float ctrlRowFactor = -20f;
+
+    Vector3 rawOffset; 
+    float xOffset, yOffset , zOffset, xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    // Use this for initialization
+    void Start () {
+        rawOffset = transform.localPosition;
+        xOffset = rawOffset.x;
+        yOffset = rawOffset.y;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isControlEnabled == true)
+        {
+            OnProcessTranslation();
+            OnProcessRotation();
+        }
+        
+    }
+
+    private void OnProcessRotation()
+    {
+        float pitch = transform.localPosition.y * posPitchFactor + yThrow * ctrlPitchFactor;
+        float yaw = transform.localPosition.x * posYawFactor ;
+        float row = xThrow * ctrlRowFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, row);
+    }
+
+    private void OnProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffsetPerFrame = Time.deltaTime * xControlSpeed * xThrow;
+        float yOffsetPerFrame = Time.deltaTime * yControlSpeed * yThrow;
+
+        xOffset = Mathf.Clamp(xOffset += xOffsetPerFrame, -xRange, xRange);
+        yOffset = Mathf.Clamp(yOffset += yOffsetPerFrame, -yRange, yRange);
+
+        transform.localPosition = new Vector3(xOffset, yOffset, rawOffset.z);
+    }
+
+    private void OnPlayerDeath()    // called by string reference
+    {
+        print("Player losing control !!!");
+        isControlEnabled = false;
+    }
+}
